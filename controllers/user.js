3ec524bff2f1d90754b38a0f1b168245f448906d
iamrashed01 <irashad42@gmail.com>
2021-06-16 19:18:18 +0600

    add admin rule

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,28 @@
 const User = require('../model/user');
 
-async function profileController(req, res) {
-  const user = await User.findById(req.user._id);
-  return res.status(200).json({
-    data: {
-      id: user._id,
-      email: user.email,
-      role: user.role,
-    },
-    message: 'user retrieved successfully',
-    success: true,
-  });
+async function profileController(req, res, next) {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(401).json({
+        data: null,
+        message: 'user couldn\'t found',
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      data: {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+      },
+      message: 'user retrieved successfully',
+      success: true,
+    });
+  } catch (err) {
+    return next(err);
+  }
 }
 
 module.exports = {
--- route/user.js
+++ route/user.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const { profileController } = require('../controllers/user');
 const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 
 /**
  * @route api/profile
  * @method GET
  */
 
-router.get('/profile', auth, profileController);
+router.get('/profile', auth, admin, profileController);
 
 module.exports = router;
